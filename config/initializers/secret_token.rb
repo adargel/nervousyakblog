e5ecde9ffc14a5370ecfed3eb25a5c009099e145
Aaron Dargel <aaron@nervousyak.com>
2011-05-26 17:28:48 -0500

    Initial import...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nervousyakblog::Application.config.secret_token = '149a917dcd88aea22bc4bd300ad0b2fd369231f1b08bfa440a38fd434d6c1a7015197a506efd0b4054738911358adc8eefc31b723c61d95be6823e848163a071'
